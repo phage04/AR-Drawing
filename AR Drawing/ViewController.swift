8a8c1ba76a544eb158f4ffd639bd659c53cf6da1
lylejover <lylejover2yahoo.com>
2018-01-26 23:35:56 -0700

    orientation & location

--- AR Drawing/ViewController.swift
+++ AR Drawing/ViewController.swift
@@ -28,7 +28,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
         print("rendering")
+        guard let pointOfView = sceneView.pointOfView else {return}
+        let transform = pointOfView.transform
+        let orientation = SCNVector3(-transform.m31,-transform.m32,-transform.m33)
+        let location = SCNVector3(transform.m41,transform.m42,transform.m44)
+        let currentPositionOfCamera = orientation + location
+        print(orientation.x, orientation.y, orientation.z)
     }
     
 }
 
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}
